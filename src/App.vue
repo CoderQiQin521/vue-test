4d03ce706ccfa4f00ad4728fed4455d86651ca1a
CoderQiQin521 <coderqiqin@aliyun.com>
2020-12-27 13:02:26 +0800

    可以

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <h1>牛逼</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. At deleniti, minima
+    itaque dicta et, saepe est facilis sequi suscipit porro molestias earum
+    veritatis, error fugit quibusdam impedit esse corporis. Voluptate?
+  </p>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
