68537eb0dd55c7ecde2b91cf513a234eab5f6b02
CoderQiQin521 <coderqiqin@aliyun.com>
2020-12-27 10:02:12 +0800

    发布第一次

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
+  <h1>123</h1>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
